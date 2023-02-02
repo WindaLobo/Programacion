5eaa6d21a6385491579ea27e78b7b83c172f32e5
lobo2 <wlobogarces@danielcastelao.org>
2023-02-02 19:14:25 +0100

    Actualizacion

---
+++ Pratica_en_casa/practica/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Metodos metodo= new Metodos();
+        //System.out.println(metodo.metodo1());
+       // metodo.metodos();
+        metodo.metodo3();
+
+
+
+
+
+    }
+}
